0cbcbc7f9ec6d303e6b5766d6535e77179d0b2ab
chenc <cheng.chen@cern.ch>
2018-09-02 16:55:47 +0200

    Raw handler

--- scripts/Algorithm_TP.py
+++ scripts/Algorithm_TP.py
@@ -1,79 +1,22 @@
-import re,os,hashlib,time,datetime
+import re,os,hashlib,time,datetime,functools
 
 import sqlite3 as sql
 import ROOT as R
-import toolkit
 
-'''
-entry:
-    sample
-    TP region
-    category
-    description:
-        nominal -> 1
-        sample variation -> 2
-        syst variation -> 3
-meta info:
-    input file
-    hash code
-    production version
-'''
-
-def filter(name):
-    #ttbar_1ptag2jet_TP_MLJ_MV2c10_Hyb_PbT60_CalJetPt
-    reg = '([^/]*/)?([^/]*)_1ptag2jet_TP_(.*)_(P[xbj][TP])([0-9][0-9])_CalJetPt_?(.*)'
-    if not re.match(reg, name):
-        return (None,)*6    
-    res = re.findall(reg, name)[0]
-    sample = res[1]
-    category = res[2] + '_' + res[4]
-    tp = res[3]
-    if res[0] == '' and res[5] == '':
-        status = 1
-        description = 'Nominal'
-        aux = None
-    elif res[0]=='Systematics/' and res[5]!='':
-        reg_pdf = 'SysPDF4LHC_([0-9]+)'
-        reg_rad = 'SysRad(High|Low)'
-        reg_syst = 'Sys(.*)__1(down|up)'
-        if re.match(reg_pdf,res[5]):
-            status = 2
-            description = 'PDF4LHC'
-            aux = re.findall(reg_pdf,res[5])[0][0]
-        elif re.match(reg_rad,res[5]):
-            status = 2
-            description = 'Radiation'
-            aux = re.findall(reg_rad,res[5])[0][0]
-        elif re.match(reg_syst,res[5]):
-            status = 3
-            description,aux = re.findall(reg_syst, res[5])[0]
-        else:
-            print name,'syst hist not known!'
-            raise ValueError()
-    else:
-        print name,'hist not known!'
-        raise ValueError()
-    return (sample,tp,category,status,description,aux)
-
-
-
-
-
-
-        
-        
-
-
-
-
-
-
-    
+def TimeCounter(fun):
+    @functools.wraps(fun)
+    def wrap(*args,**kw):
+        start = time.time()
+        fun(*args,**kw)
+        duration = time.time() - start
+        print '<{0:}> -- {1:.2f}'.format(fun.__name__,duration)
 
 class Root_Histogram_Handler(object):
+    isDebug = False
     def __init__(self,path_to_root_file,version):
         self.version = version
         self.path_to_root_file = path_to_root_file
+
 
     def SetFilter(self,fun):
         self.filter = fun
@@ -83,7 +26,9 @@ class Root_Histogram_Handler(object):
         self.str_xbins = '|'.join(map(str,xbins))
 
 
-    def Digest(self,path_to_data_base,overWrite=False):
+    def Digest(self,path_to_data_base):
+        if Root_Histogram_Handler.isDebug:
+            os.system('rm %s'%(path_to_data_base))
         self.DB = sql.connect(path_to_data_base)        
         self._write_meta_data()
         self._write_histograms()
@@ -94,17 +39,31 @@ class Root_Histogram_Handler(object):
         if version!=None:
             cursor.execute('''
 SELECT * FROM MetaData
-WHERE Version=?
-            ''',(version))
+WHERE Version=?;
+            ''',(version,))
         else:
             cursor.execute('''
 SELECT * FROM MetaData
             ''')
         itms = cursor.fetchall()
         if len(itms) == 0:
-            return None
+            return []
         else:
-            return itms
+            res = list(itms)
+            return res
+
+    def _print_meta(self,meta_data):
+        header = self._get_header('MetaData')
+        for n,itm in enumerate(meta_data):
+            print '{0:<15} {1:}'.format(header[n]+':',itm)
+
+    def _get_header(self,table_name):
+        cursor = self.DB.cursor()
+        cursor.execute('''
+            SELECT * FROM {0:}
+            '''.format(table_name))
+        header = [ key[0] for key in cursor.description ]
+        return header
 
     def _write_meta_data(self):
         cursor = self.DB.cursor()
@@ -116,30 +75,36 @@ CREATE TABLE IF NOT EXISTS MetaData(
     InputFile TEXT NOT NULL,
     HashValue TEXT NOT NULL
 );                                                                     
-        ''')
+        ''')        
+        meta = self.GetMetaData(self.version)
+        if len(meta)!=0:
+            print 'Version <{0:}> exists already!'
+            self._print_meta(meta[0])
+            raise ValueError()
         with open(self.path_to_root_file,'rb') as f:
             chunk = f.read()
             hasher = hashlib.md5()
             hasher.update(chunk)
             hash_value = hasher.hexdigest()
-        str_time = datetime.utcnow().strftime("%Y-%m-%d,%a,%H:%M:%S UTC+0")
-        meta = self.GetMetaData(self.version)
-        if meta != None:
-            print 'Table exists already!'
-            for itm in meta:
-                print itm
-            raise ValueError()
+        str_time = datetime.datetime.utcnow().strftime("%Y-%m-%d,%a,%H:%M:%S UTC+0")
         cursor.execute('''
 INSERT INTO MetaData(Version,Binning,ImportTime,InputFile,HashValue) \
 VALUES(?,?,?,?,?);
         ''',(self.version,self.str_xbins,str_time,self.path_to_root_file,hash_value))
         meta = self.GetMetaData(self.version)
-        for itm in meta:
-            print itm
+        print 'This input going to be processed'
+        self._print_meta(meta[0])
 
     def _write_histograms(self):
         self._initialize_table()
         f = R.TFile(self.path_to_root_file,'read')
+        self.entries = []
+        self._walker(f)
+        cursor = self.DB.cursor()
+        cursor.executemany('''
+INSERT INTO Data_{version:}({keys:}) \
+VALUES({question_marks});
+'''.format(version=self.version,keys=self.str_keys,question_marks=self.str_question_marks),self.entries)
 
     def _initialize_table(self):   
         cursor = self.DB.cursor()
@@ -153,7 +118,7 @@ Error_{0:} REAL NOT NULL,
             '''.format(nbin)
             keys += ',Value_{0:},Error_{0:}'.format(nbin)
         self.str_keys = keys
-        self.str_question_marks = '?,?,?,?,?'+(',?,?'*nbins)
+        self.str_question_marks = '?,?,?,?,?,?'+(',?,?'*nbins)
         cmd = '''
 CREATE TABLE Data_%s(
     Sample TEXT NOT NULL,
@@ -164,7 +129,7 @@ CREATE TABLE Data_%s(
     Aux TEXT,
     %s
     );
-        '''%(self.version,str_value_error[:-2])
+        '''%(self.version,str_value_error.strip(' ')[:-2])        
         cursor.execute(cmd)
 
     def _walker(self,root_dir,prefix=''):
@@ -179,27 +144,25 @@ CREATE TABLE Data_%s(
             else:
                 continue
 
-    def _write_entry(name,key):
+    def _write_entry(self,name,key):
         sample,tp,category,status,description,aux = self.filter(name)        
         if status!=None:
             value_errors = self._read_hist(key)
             values = (sample,tp,category,status,description,aux) + value_errors
             cursor = self.DB.cursor()
-            cursor.execute('''
-SELECT * FROM Data_{version:}
-WHERE Sample={sample:} AND TP={tp:} AND Category={category:} AND Status={status:} AND description={description:} AND aux={aux:}
-            '''.format(version=self.version,sample=sample,tp=tp,category=category,status=status,description=description,aux=aux))
-            entries = cursor.fetchall()
-            if len(entries)>=1:
-                for entry in entries:
-                    print entry
-                print name
-                raise ValueError("entry already exists!")
-            cursor.execute('''
-INSERT INTO Data_{version:}({keys:}) \
-VALUES({question_marks});
-            '''.format(version=self.version,keys=self.str_keys,question_marks=self.str_question_marks),values)
-    
+            if False:
+                cursor.execute('''
+    SELECT * FROM Data_{version:}
+    WHERE Sample=? AND TP=? AND Category=? AND Status=? AND Description=? AND Aux=?;
+                '''.format(version=self.version),(sample,tp,category,status,description,aux))
+                entries = cursor.fetchall()
+                if len(entries)>=1:
+                    for entry in entries:
+                        print entry
+                    print name
+                    raise ValueError("entry already exists!")
+            self.entries.append(values)
+
     def _read_hist(self,key):
         hist = key.ReadObj()
         values = ()
--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-DIR=$PWD/$(cd $(dirname ${BASH_SOURCE[0]})&&pwd)
+DIR=$(cd $(dirname ${BASH_SOURCE[0]})&&pwd)
 export PYTHONPATH=$DIR/scripts/:$DIR/HEP_pyLib:$PYTHONPATH
-
+if [[ ${ATLAS_LOCAL_ROOT_BASE} != "" ]];then
+    setupATLAS
+    lsetup "python 2.7.9p1-x86_64-slc6-gcc49"
+    lsetup "root 6.08.06-x86_64-slc6-gcc49-opt"
+fi
